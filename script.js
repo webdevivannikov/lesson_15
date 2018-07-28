e24e59d52db608ea69b1e2ef3313bf315f4a792c
Sanek <polkil0@yandex.ru>
2018-07-28 22:15:28 +0700

    First Commit

---
+++ script.js
@@ -0,0 +1,30 @@
+var main = document.getElementById("main");
+var myList = document.getElementById("list");
+var btn1 = document.getElementById("btn1");
+var btn2 = document.getElementById("btn2");
+var story = myList.getElementsByClassName("item");
+var popup = document.getElementsByClassName("popup")[0];
+var close = document.querySelector(".close");
+
+function addItem(){
+	var newLi = document.createElement("li");
+	newLi.innerHTML = "Новая задача";
+	newLi.className = "item";
+	myList.appendChild(newLi);
+	popup.style.display = "none";
+}
+function delItem(){
+	myList.removeChild(story[story.length-1]);
+
+	if(story.length == 0){
+		popup.style.display = "block";
+	}
+	
+}
+function closePopup() {
+	popup.style.display = "none";
+}
+
+btn1.addEventListener("click", addItem);
+btn2.addEventListener("click", delItem);
+close.addEventListener("click", closePopup);
